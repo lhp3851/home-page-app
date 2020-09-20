48eb891b1c6f273eb70295f23e2cbc1553b7ac3d
Jerry <lhp3851@163.com>
2020-09-20 10:41:13 +0800

    [feature][Project]: initial and config project

---
+++ src/Pages/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import UserProfile from "../User";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UserProfile />
+      </header>
+    </div>
+  );
+}
+
+export default App;
