7e4eabd3f5a562053495d98eb4254e34c16f4e4c
Jerry <lhp3851@163.com>
2020-10-03 00:47:17 +0800

    Updates

---
+++ precache-manifest.8f11d44ffaa911ae4a4d66e0424d7e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b0d51600a5889ffc287b2c0faa4660",
+    "url": "/home-page-app/index.html"
+  },
+  {
+    "revision": "fd072a0bd2319f29ea9c",
+    "url": "/home-page-app/static/css/main.d2928d36.chunk.css"
+  },
+  {
+    "revision": "467c8b5f45ec511b755c",
+    "url": "/home-page-app/static/js/2.36d89752.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home-page-app/static/js/2.36d89752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd072a0bd2319f29ea9c",
+    "url": "/home-page-app/static/js/main.efb140e6.chunk.js"
+  },
+  {
+    "revision": "fd28e10a54fe9f3b771e",
+    "url": "/home-page-app/static/js/runtime-main.fe4ce84c.js"
+  }
+]);
